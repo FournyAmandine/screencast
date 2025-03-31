89a7da91b3af6060c81eb822bc97dba46bcd46c3
FournyAmandine <amandine.fourny@student.hepl.be>
2025-03-31 15:29:20 +0200

    Updated code

--- core/Response.php
+++ core/Response.php
@@ -4,13 +4,22 @@ namespace Tecgdcs;
 
 class Response
 {
-    public static function abort(){
-        die('Oups un problème technique est survenu');
+    const BAD_REQUEST = 400;
+    const UNAUTHORIZED = 401;
+    const NOT_FOUND = 404;
+    const SEE_OTHER = 303;
+    const SERVER_ERROR = 500;
+    public static function abort($code = self::NOT_FOUND){
+        http_response_code($code);
+        require CODES.$code.'view.php';
     }
     public static function redirect($url){
         header('Location: '.$url);
+        self::abort(self::SERVER_ERROR);
+        exit();
     }
     public static function back(){
         self::redirect($_SERVER['HTTP_REFERER']);
+        self::abort(self::SERVER_ERROR);
     }
 }
